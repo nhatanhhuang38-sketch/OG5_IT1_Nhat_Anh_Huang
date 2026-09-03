666254462e1e59cc8799c2672128fc55a6252a4a
user <user@192.168.34.113>
2026-09-03 10:09:18 +0200

    Taschenrechner implementiert

---
+++ Taschenrechner/src/Taschenrechner.java
@@ -0,0 +1,21 @@
+
+
+public class Taschenrechner {
+
+	public double add(double zahl1, double zahl2) {
+		return zahl1 + zahl2;
+	}
+	
+	public double sub(double zahl1, double zahl2){
+		return zahl1 - zahl2;
+	}
+	
+	public double mul(double zahl1, double zahl2){
+		return zahl1 * zahl2;
+	}
+
+	public double div(double zahl1, double zahl2){
+		return zahl1 / zahl2;
+	}
+	
+}
